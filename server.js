b871973b1f9bc4bd706cabbd72fb8298707fecc9
Shawn Broyles <35933953+ShawnBroyles@users.noreply.github.com>
2023-02-09 14:07:25 -0500

    Web hosting capabilities added via Express.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', function(request, response) {
+	response.send('Welcome to the main page. Check out the <a href="/welcome">welcome</a> page.');
+});
+
+app.get('/welcome', function(request, response) {
+	response.sendFile(__dirname + '/public/welcome.html');
+});
+
+app.listen(PORT, ()=> console.log('\nEnsure that port 3000 is port-forwarded to allow connections over the internet.\nThe server will stay alive until this command prompt is closed.\n\nServer running on port 3000.\n'));
