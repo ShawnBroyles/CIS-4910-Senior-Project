3095709f4931a2dc60c094e7c5f3bd8ad3689035
Shawn Broyles <35933953+ShawnBroyles@users.noreply.github.com>
2023-02-09 14:47:48 -0500

    Images are now visible to website visitors

--- server.js
+++ server.js
@@ -10,4 +10,7 @@ app.get('/welcome', function(request, response) {
 	response.sendFile(__dirname + '/public/welcome.html');
 });
 
+app.use(express.static('public')); 
+app.use('/images', express.static('images'));
+
 app.listen(PORT, ()=> console.log('\nEnsure that port 3000 is port-forwarded to allow connections over the internet.\nThe server will stay alive until this command prompt is closed.\n\nServer running on port 3000.\n'));
